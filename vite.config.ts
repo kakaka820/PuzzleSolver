d60163dfcf40fd16ae06b7353cf3a0165745633f
kakaka820 <tata_ta@163.com>
2026-02-02 12:45:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [react()],
   resolve: {
     alias: {
