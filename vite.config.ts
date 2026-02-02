623f23552a35abb95249ffe22964a2059a8e24b8
kakaka820 <tata_ta@163.com>
2026-02-02 16:43:50 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
   plugins: [react()],
   resolve: {
     alias: {
