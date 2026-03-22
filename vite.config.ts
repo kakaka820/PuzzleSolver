71c5308e2c739201df5fb1f1f38bf08047c1c810
kakaka820 <tata_ta@163.com>
2026-03-22 15:58:54 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
